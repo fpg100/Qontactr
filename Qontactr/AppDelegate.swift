b9fea29869a75d89038209fd5a90f355ae003e8a
jccherry <jchiaramonte18@regis.org>
2016-09-07 00:56:46 -0400

    fixed analytics

--- Qontactr/AppDelegate.swift
+++ Qontactr/AppDelegate.swift
@@ -21,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         GADMobileAds.configureWithApplicationID("ca-app-pub-7526118464921133~6520412802")
+        FIRApp.configure()
         return true
     }
 
